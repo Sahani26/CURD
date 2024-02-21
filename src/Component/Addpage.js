bada96a03e60369c3ff014a7b736f3320ddffd60
Sahani26 <sunilsahani484@gmail.com>
2024-02-21 12:03:01 +0530

    add delete methode

--- src/Component/Addpage.js
+++ src/Component/Addpage.js
@@ -1,29 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Addpage() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const tit = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const fullname1 = (event) => {
+    setName(event.target.value);
+  };
+
+  const pr = (event) => {
+    setPrice(event.target.value);
+  };
+
+  const email1 = (event) => {
+    setEmail(event.target.value);
+  };
+
+  function done (){
+// console.warn({name,email,price,title});
+let data = {name,email,price,title}
+fetch("http://localhost:3000/posts",{
+  method:'POST',
+  headers:{
+    'Accept':'application/json',
+    'Content-Type': 'application/json'
+  },
+  
+  body:JSON.stringify(data)
+
+}).then((result)=>{
+
+}) 
+
+  }
   return (
-   <>
-   
-   <div className="container my-3">
-   <form>
-   <div className="form-group">
-    <label htmlFor="exampleInputEmail1">Full Name</label>
-    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your full name"/>
-    <small id="emailHelp" className="form-text text-muted"></small>
-  </div>
-  <div className="form-group">
-    <label htmlFor="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" className="form-text text-muted"></small>
-  </div>
-  <div className="form-group">
-    <label htmlFor="exampleInputPassword1">Enter Mobile</label>
-    <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Mobile no."/>
-  </div>
- 
-  <button type="submit" className="btn btn-primary my-3">Submit</button>
-</form>
-    </div>
-   </>
+    <>
+
+      <div className="container my-3">
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">title</label>
+            <input type="text" value={title} onChange={tit} name="title" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your full name" />
+            <small id="emailHelp" className="form-text text-muted"></small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Full Name</label>
+            <input type="text"  onChange={fullname1} value={name} name="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your full name" />
+            <small id="emailHelp" className="form-text text-muted"></small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" value={email}  onChange={email1} name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            <small id="emailHelp" className="form-text text-muted"></small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">price</label>
+            <input type="text" on value={price}  onChange={pr} name="price" className="form-control" id="exampleInputPassword1" placeholder="price" />
+          </div>
+
+          <button type="button" onClick={done} className="btn btn-primary my-3">Submit</button>
+        </form>
+      </div>
+    </>
   )
 }
--- src/Component/Homepage.js
+++ src/Component/Homepage.js
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from 'react'
-// import axios from 'axios';
+
 export default function Homepage() {
+// when we call api then alwayes use fetch method // and url put into the fetch
+//fetch("http://localhost:3000/posts") when we use this code then it alwayes rreturn a promish
+//.then((result)use in promish//
+//useEffect call should be use alwayes useEffect
 
   const [users, setUser] = useState([])
   useEffect(() => {
-    fetch("http://localhost:3000/posts").then((result) => {
-      result.json().then((resp) => {
-        // console.warn("result",resp)
-        setUser(resp)
-      })
-    })
+    getlist()
   }, [])
-  
+function getlist(){
+  fetch("http://localhost:3000/posts").then((result) => {
+    result.json().then((resp) => {
+      // console.warn("result",resp)
+      setUser(resp)
+    })
+  })
+}
+  function deleteuser(id){
+    fetch(`http://localhost:3000/posts/${id}`,{
+      method:'DELETE'
+    }).then((resp) => {
+      console.warn("result",resp)
+      getlist()
+      // setUser(resp)
+    })
+  }
+
   return (
     <>
       <div className="container my-3">
@@ -34,7 +50,7 @@ export default function Homepage() {
                   <td>{item.price}</td>
                   <td>{item.name}</td>
                   <td>{item.email}</td>
-                  <td></td>
+                  <td><button onClick={()=>deleteuser(item.id)}>delete</button></td>
                 </tr>
               )
             }
